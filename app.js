90ec108bf28926d3c9c75357f3d8a43623b371d7
gosurvashi29 <urvashigoswami350@gmail.com>
2025-02-25 20:13:49 +0530

    express with different routes

---
+++ app.js
@@ -0,0 +1,35 @@
+// Import Express
+const express = require('express');
+
+// Initialize the Express app
+const app = express();
+
+// Use middleware to parse JSON data for POST requests
+app.use(express.json());
+
+// Define routes
+
+// GET /orders
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
+});
+
+// POST /orders
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
