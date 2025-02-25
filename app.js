3926690a5f893f674508fc18bca571fb789b6d3d
gosurvashi29 <urvashigoswami350@gmail.com>
2025-02-25 20:19:11 +0530

    The wildcard route for handling 404 errors

--- app.js
+++ app.js
@@ -29,6 +29,12 @@ app.post('/users', (req, res) => {
   res.send('A new user has been added.');
 });
 
+
+// Wildcard route to handle undefined routes
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
+  });
+  
 // Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
